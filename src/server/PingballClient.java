7f4ba54f6d345587d990c7086f6cd83814266c4d
Owen Trueblood <jmptable@mit.edu>
2014-05-07 12:42:44 -0400

    Unused import.

--- src/server/PingballClient.java
+++ src/server/PingballClient.java
@@ -3,7 +3,6 @@ package server;
 import game.Board;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
